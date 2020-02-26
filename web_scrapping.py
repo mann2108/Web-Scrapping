c2e86d4aaf1f2b7729b13c9a7ffefcbbfe404ad2
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-02-27 01:59:40 +0530

    Update web_scrapping.py

--- web_scrapping.py
+++ web_scrapping.py
@@ -9,20 +9,20 @@ Task :  downloading all the solution codes from codechef practice section locate
 Prerequisite Actions : -
 
 1. Install Selenium globally using - 'pip install selenium'
-   If you are using pycharm then also install in it too for hint feature.
+   If you are using pycharm then also install there for hint feature.
    Pycharm Installation : file->settings->Project Interpreter->click '+'->write selenium in search bar->Click Installed.
 
 2. Create 'Problems' directory in same directory where this file exist.
 
 3. Now download the chromedriver from the given link and just give the path to it in below code.
    In case some error occur due to version then try all different version of chrome browser you are
-   using and select accordingly.
+   using and select accordingly. ('https://chromedriver.chromium.org/downloads')
 
 4. Count the no. of problems you have in practice problem section on codechef and assign it to for loop.
 
 5. Give your profile url in 'driver.get()' method.
 
-6. Lastly after getting text from line 37 give the extension of file name as '.py' or '.cpp' whatever your
+6. Lastly after getting text from line 37 give the extension of file name as '.py', '.cpp', '.java', etc. whatever language your
    written in.
 
 **********Happy Coding and Scrapping**********
